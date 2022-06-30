6709b07aa3c2df590c2ef664ffbe91a378681148
b1u3too <almadsen792@live.ca>
2022-06-30 05:21:07 +0000

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,7 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "The total sales are $#{Store.sum('annual_revenue')}.00."
+puts "The averages sales across all stores are $#{Store.average('annual_revenue').round(2)}."
+@top_sellers = Store.where(["annual_revenue >= :annual_revenue", {annual_revenue: 1000000}]) 
+puts "The number of top selling stores is #{@top_sellers.count}."
